0b3218ec3905e9a7dda3ea2dc630f59643e6e5d6
Angela Carraro <angela_carraro@hotmail.it>
2020-11-16 17:38:54 +0100

    Done FizzBuzz

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,49 @@
 package dssc.assignment.fizzbuzz;
 
+
+/**
+ * FizzBuzz assignment
+ * We want to write a Java program that prints the numbers from 1 to 100 to the
+ * "standard" output stream and follows these rules.
+ *
+ * For multiples of 3 it prints Fizz instead of the number.
+ * For multiples of 5 it prints Buzz instead of the number.
+ * For numbers which are multiples of both 3 and 5 it prints FizzBuzz.
+ */
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+
+        for(int i = 1; i <= 100; i += 1){
+            if (i % 3 == 0){
+                System.out.printf("Fizz");
+            }
+            if (i % 5 == 0){
+                System.out.printf("Buzz");
+            } else {
+                if (i % 3 != 0) {
+                    System.out.printf("" + i);
+                }
+            }
+            System.out.println("");
+        }
+
+        // Interesting version
+//        boolean flag = true;
+//        for(int i=0;i<16;i++){
+//            if(i%3==0){
+//                System.out.print("Fizz");
+//                flag=false;
+//            }
+//            if(i%5==0){
+//                System.out.print("Buzz");
+//                flag=false;
+//            }
+//            if (flag)
+//                System.out.print(i);
+//            System.out.print(",");
+//            flag = true;
+//        }
     }
 
 }
