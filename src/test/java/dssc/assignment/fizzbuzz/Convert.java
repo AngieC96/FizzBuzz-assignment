9210e3253de039163b323e4dc826b1368a5d06e1
Angela Carraro <angela_carraro@hotmail.it>
2020-11-19 08:43:24 +0100

    Refactor tests

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -1,10 +1,8 @@
 package dssc.assignment.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,21 +35,30 @@ public class Convert {
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
-    @Test
-    void number_3_to_Fizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(3));
-    }
+//    @Test
+//    void number_3_to_Fizz() {
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//        assertEquals("Fizz", fizzBuzz.convert(3));
+//    }
+//
+//    @Test
+//    void number_6_to_Fizz() {
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//        assertEquals("Fizz", fizzBuzz.convert(6));
+//    }
+//
+//    @Test
+//    void number_24_to_Fizz() {
+//        FizzBuzz fizzBuzz = new FizzBuzz();
+//        assertEquals("Fizz", fizzBuzz.convert(24));
+//    }
 
-    @Test
-    void number_6_to_Fizz() {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 24})
+    // Since we need to provide only one argument to our test
+    void multiples_of_3_to_Fizz(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(6));
-    }
-
-    @Test
-    void number_24_to_Fizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(24));
+        assertEquals("Fizz", fizzBuzz.convert(number));
     }
 }
+
