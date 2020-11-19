faeec852b6e0f8ef8acc960223cc808001752995
Angela Carraro <angela_carraro@hotmail.it>
2020-11-19 08:33:20 +0100

    We now convert 6 to 'Fizz'

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -51,7 +51,10 @@ public class FizzBuzz {
         //return "1";
         if (number == 3) {
             return "Fizz";
+        } else if (number == 6) {
+            return "Fizz";
+        } else {
+            return Integer.toString(number);
         }
-        return Integer.toString(number);
     }
 }
